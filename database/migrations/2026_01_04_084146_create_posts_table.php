34af208187d0933ac05ffc7a7e318315a59cabf6
Mohammed Bashamkha <mb877402@gmail.com>
2026-01-09 16:41:41 +0300

    make title and content fields nullable in posts migration and add type

--- database/migrations/2026_01_04_084146_create_posts_table.php
+++ database/migrations/2026_01_04_084146_create_posts_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('title');
-            $table->text('content');
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            $table->enum('type', ['text', 'image', 'video'])->default('text');
             $table->softDeletes();
             $table->timestamps();
         });
